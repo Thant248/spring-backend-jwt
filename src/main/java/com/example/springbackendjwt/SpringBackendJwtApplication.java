1e3894785cb71b17ec2b39d12448660295023d7c
Thant248 <thantsinphyoe18920@gmail.com>
2023-09-01 15:48:58 +0630

    Initial commit

---
+++ src/main/java/com/example/springbackendjwt/SpringBackendJwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbackendjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBackendJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbackendjwt/SpringBackendJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbackendjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBackendJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
